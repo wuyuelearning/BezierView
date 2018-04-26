d10742f8a58eef3da3a68823de4bd9d1ef58b150
wuyue <wuyue@lvmama.com>
2018-04-26 19:46:03 +0800

    添加回弹效果

--- app/src/main/java/com/example/admin/beziertest/BezierView2.java
+++ app/src/main/java/com/example/admin/beziertest/BezierView2.java
@@ -1,5 +1,10 @@
 package com.example.admin.beziertest;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -10,6 +15,9 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AnticipateOvershootInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.LinearInterpolator;
 
 /**
  * Created by wuyue on 2018/3/23.
@@ -18,6 +26,7 @@ import android.view.View;
 public class BezierView2 extends View {
 
     private static final String TAG = "tag";
+    private static final String PATH = "path";
 
     Point startPoint1; //曲线1
     Point endPoint1;
@@ -73,8 +82,7 @@ public class BezierView2 extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-
+        Log.d(TAG, "onDraw");
         ensureBezierPoint();
         path.reset();
         //  六行代码 使画笔形的轨迹成封闭的图形，使得可以填充满颜色
@@ -96,20 +104,53 @@ public class BezierView2 extends View {
 //        path.quadTo(middlePoint2.x, middlePoint2.y, endPoint2.x, endPoint2.y);
 //        path.lineTo(endPoint2.x, endPoint2.y);
 
-        //  小圆消失后 贝塞尔曲线也消失
+        //  小圆消失后或两个 贝塞尔曲线也消失
+
         if (isShowFixedCircle) {
             canvas.drawPath(path, paint);
-            canvas.drawCircle(fixedCircle.x, fixedCircle.y, fixedCircle.r, paintCircle);   //   固定小圆
+            canvas.drawCircle(fixedCircle.x, fixedCircle.y, fixedCircle.r, paintCircle);   //   固定圆
+//            Log.d(PATH,"isShowFixedCircle");
+        }
+
+        float distance = getDistance(touchCircle.x, touchCircle.y);
+        // 如果两圆之间的距离小于两圆半径（固定圆半径会变）之和，则贝塞尔曲线消失
+        if ((touchCircle.r + fixedCircle.r) >= distance) {
+//            Log.d(PATH,"distance");
+            canvas.drawPath(path, paint);
         }
         canvas.drawCircle(touchCircle.x, touchCircle.y, touchCircle.r, paintCircle);    //    动圆
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Log.d(TAG, "onMeasure");
+
         screenWidth = MeasureSpec.getSize(widthMeasureSpec);
         screenHeight = MeasureSpec.getSize(heightMeasureSpec);
+
+        Log.d(TAG, "onMeasure" + widthMeasureSpec);
+        Log.d(TAG, "onMeasure" + heightMeasureSpec);
+        Log.d(TAG, "onMeasure" + screenWidth);
+        Log.d(TAG, "onMeasure" + screenHeight);
+        Log.d(TAG, "onMeasure" + MeasureSpec.getMode(widthMeasureSpec));
+        Log.d(TAG, "onMeasure" + MeasureSpec.getMode(heightMeasureSpec));
+        Log.d(TAG, "onMeasure" + MeasureSpec.makeMeasureSpec(MeasureSpec.getSize(widthMeasureSpec), MeasureSpec.getMode(widthMeasureSpec)));
+        Log.d(TAG, "onMeasure" + MeasureSpec.makeMeasureSpec(MeasureSpec.getSize(heightMeasureSpec), MeasureSpec.getMode(heightMeasureSpec)));
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         init();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left + 100, top + 100, right, bottom);
+
+        Log.d(TAG, "onLayout  " + getMeasuredWidth());
+        Log.d(TAG, "onLayout  " + getMeasuredHeight());
+        Log.d(TAG, "onLayout  " + left);
+        Log.d(TAG, "onLayout  " + top);
+        Log.d(TAG, "onLayout  " + right);
+        Log.d(TAG, "onLayout  " + bottom);
+        Log.d(TAG, "onLayout  ");
     }
 
     private void init() {
@@ -125,6 +166,8 @@ public class BezierView2 extends View {
 
         initEndX = screenWidth / 5;
         initEndY = screenHeight / 5;
+
+        Log.d(PATH, "" + initEndX + "  " + initEndY);
 
         fixedCircle = new Circle();
         touchCircle = new Circle();
@@ -154,6 +197,7 @@ public class BezierView2 extends View {
         paintCircle = new Paint(Paint.ANTI_ALIAS_FLAG);
 //        paintCircle.setStyle(Paint.Style.FILL);
         paintCircle.setColor(Color.rgb(0xaa, 0xee, 0xee));
+
 
     }
 
@@ -197,19 +241,92 @@ public class BezierView2 extends View {
             isShowFixedCircle = false;
             onceDisppeared = true;
         }
+//        Log.d("flag","motionMove");
         invalidate();
     }
 
     private void motionUp() {
         //isShowFixedCircle = false;
-        //  抬起点击点  大圆退回原位
-        touchCircle.x = initEndX;
-        touchCircle.y = initEndY;
+        Log.d(PATH, "motionUp  " + touchCircle.x + "  " + touchCircle.y);
+        if (isShowFixedCircle) {
+            // 当小圆未消失的情况下，松开控制点 ，显示回弹效果
+            startAnimation();
+        } else {
+            //  抬起点击点  大圆退回原位
+            touchCircle.x = initEndX;
+            touchCircle.y = initEndY;
+            reset();
+        }
+        invalidate();
+    }
+
+    private void reset() {
         ensureBezierPoint();
         ensureMiddlePoint();
         isShowFixedCircle = true;
         onceDisppeared = false;
-        invalidate();
+    }
+
+    private void startAnimation() {
+
+
+        // ofFloat 第一个参数是x的起始位置（数值touchCircle.x，仅仅是因为此时控制点的x坐标与touchCircle.x数值一直），
+        // 第二个为终点位置
+        ValueAnimator animatorX = ValueAnimator.ofFloat(touchCircle.x, initEndX);  //  语句1
+        animatorX.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                //循环地将x坐标位置赋值给 固定球的x坐标，从而得到固定球x的轨迹
+                //  语句1是一个轨迹，一种变化 ，如果没有给哪个视图赋值 将没有什么意义，此时赋值给了touchCircle.x
+                //  也就是使得控制圆的x坐标的变化与animatorX的变化 一致，
+                // 所以 ，可以看成先在animatorX中设置变化内容 ，再将这种变化赋值给某个视图控件，是这种变化
+                // 与视图控价联系起来，使视图也按照animatorX的变化而变化
+                touchCircle.x = (float) animation.getAnimatedValue();
+//                Log.d(PATH, "startAnimation X: " + touchCircle.x);
+                ensureBezierPoint();
+                ensureMiddlePoint();
+                invalidate();
+            }
+        });
+
+
+        // ofFloat 第一个参数是y的起始位置，第二个为终点位置
+        ValueAnimator animatorY = ValueAnimator.ofFloat(touchCircle.y, initEndY);
+        animatorY.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                touchCircle.y = (float) animation.getAnimatedValue();
+//                Log.d(PATH,"startAnimation Y: "+touchCircle.y);
+                //  需要不断更新 ，否则可能会出现贝塞尔曲线动画滞后的现象
+                ensureBezierPoint();
+                ensureMiddlePoint();
+                invalidate();
+            }
+        });
+
+        Log.d(PATH, "startAnimation  " + touchCircle.x + "  " + touchCircle.y);
+
+        //  设置动画合集
+        AnimatorSet animatorSet = new AnimatorSet();
+
+        // animatorX,animatorY 同时变化
+        animatorSet.play(animatorX).with(animatorY);
+//        animatorSet.setDuration(700);
+//        animatorSet.setInterpolator(new LinearInterpolator());
+        //  两种回弹效果
+//        animatorSet.setInterpolator(new AnticipateOvershootInterpolator());
+        animatorSet.setInterpolator(new BounceInterpolator());
+        animatorSet.start();
+
+        // 在动画结束后执行
+        animatorSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                reset();
+            }
+        });
+
     }
 
     //判断点击点是否在圆的范围内
@@ -236,6 +353,7 @@ public class BezierView2 extends View {
         middlePoint1.x = middlePoint2.x = (fixedCircle.x + touchCircle.x) / 2;
         middlePoint1.y = middlePoint2.y = (fixedCircle.y + touchCircle.y) / 2;
 //        }
+//        Log.d(PATH,""+middlePoint1.x+"   "+middlePoint1.y);
     }
 
     /**
